0c957686472aa2419463585fb7940911db944515
Graeme Smecher <gsmecher@threespeedlogic.com>
2023-01-30 11:16:29 -0800

    Add Tuber package:
    	https://github.com/gsmecher/tuber
    
    Tuber is a C++ server and Python client for exposing an instrumentation
    control plane across a network.

---
+++ package/tuber/tuber.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# Tuber Server
+#
+################################################################################
+
+TUBER_VERSION = v0.5
+TUBER_SITE_METHOD = git
+TUBER_SITE = ssh://git@github.com/gsmecher/tuber.git
+TUBER_LICENSE = GPL-3.0
+TUBER_LICENSE_FILES = LICENSE
+TUBER_SUPPORTS_IN_SOURCE_BUILD = YES
+TUBER_INSTALL_STAGING = YES
+
+TUBER_CONF_OPTS = -DPYTHON_EXECUTABLE=$(HOST_DIR)/bin/python3
+
+TUBER_DEPENDENCIES = python-pybind boost libhttpserver fmt
+
+$(eval $(cmake-package))
