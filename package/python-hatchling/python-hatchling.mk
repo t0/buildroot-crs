21f0854fe03001973238393e95b152d4ccde4a93
Graeme Smecher <gsmecher@threespeedlogic.com>
2023-05-02 13:46:40 -0700

    Upgrade Python hatchling and typing packages in anticipation of jupyterlab.

--- package/python-hatchling/python-hatchling.mk
+++ package/python-hatchling/python-hatchling.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_HATCHLING_VERSION = 1.11.1
+PYTHON_HATCHLING_VERSION = 1.14.1
 PYTHON_HATCHLING_SOURCE = hatchling-$(PYTHON_HATCHLING_VERSION).tar.gz
-PYTHON_HATCHLING_SITE = https://files.pythonhosted.org/packages/24/20/3e21d2bc57229822ac9fb9b314d7892c16f829f34a0eb247c55fc11e09a8
+PYTHON_HATCHLING_SITE = https://files.pythonhosted.org/packages/aa/d2/613087e785e4631a96d8b53688e28d5cd55a67caee9000e6408c0c58bcd2
 PYTHON_HATCHLING_LICENSE = MIT
 PYTHON_HATCHLING_LICENSE_FILES = LICENSE.txt
 PYTHON_HATCHLING_SETUP_TYPE = pep517
@@ -14,6 +14,7 @@ HOST_PYTHON_HATCHLING_DEPENDENCIES = \
 	host-python-editables \
 	host-python-packaging \
 	host-python-pathspec \
-	host-python-pluggy
+	host-python-pluggy \
+	host-python-trove-classifiers
 
 $(eval $(host-python-package))
