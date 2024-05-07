da1855426bd35ff67721e71ce08e47c48afdf46c
Graeme Smecher <gsmecher@t0.technology>
2024-05-07 15:59:23 -0700

    jupyterlab fixup: jupyter-server relies on host-nodejs

--- package/jupyter-server/jupyter-server.mk
+++ package/jupyter-server/jupyter-server.mk
@@ -9,7 +9,7 @@ JUPYTER_SERVER_SITE = $(call github,jupyter-server,jupyter_server,$(JUPYTER_SERV
 JUPYTER_SERVER_SETUP_TYPE = pep517
 JUPYTER_SERVER_LICENSE = BSD-3-Clause
 JUPYTER_SERVER_LICENSE_FILES = LICENSE
-JUPYTER_SERVER_DEPENDENCIES = host-python-hatch-jupyter-builder python-overrides
+JUPYTER_SERVER_DEPENDENCIES = host-python-hatch-jupyter-builder python-overrides host-nodejs
 JUPYTER_SERVER_SETUP_TYPE = pep517
 
 $(eval $(python-package))
