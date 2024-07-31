83fb54c5ef6fceffe75e8752dcabe0f982410f03
Graeme Smecher <gsmecher@t0.technology>
2024-07-31 13:38:12 -0700

    Uprev tuber 0.13 -> tuberd 0.15

---
+++ package/tuberd/tuberd.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# Tuber Server
+#
+################################################################################
+
+TUBERD_VERSION = 0.15
+TUBERD_SITE = https://files.pythonhosted.org/packages/56/dd/1cebac2f56b637903135233566758909c867621833c912583cc8fbe02422
+TUBERD_LICENSE = BSD-3-Clause
+TUBERD_LICENSE_FILES = LICENSE
+TUBERD_INSTALL_STAGING = YES
+TUBERD_SETUP_TYPE = setuptools
+TUBERD_DEPENDENCIES = python-pybind python-cbor2 python-orjson libhttpserver fmt host-python-setuptools-scm
+TUBERD_ENV = CMAKE_ARGS="-DCMAKE_TOOLCHAIN_FILE=$(HOST_DIR)/usr/share/buildroot/toolchainfile.cmake -DCMAKE_MODULE_PATH=$(STAGING_DIR)/usr/share/cmake/Modules -Dpybind11_DIR=$(PYTHON_PYBIND_INSTALL_PATH)/share/cmake/pybind11"
+
+$(eval $(python-package))
